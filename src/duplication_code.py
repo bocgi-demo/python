8954bc7b7870914f809aa484ed06ec83e267ab72
xxc <x007007007@hotmail.com>
2020-06-05 05:05:05 +0000

    add duplication terrible

---
+++ src/duplication_code.py
@@ -0,0 +1,15 @@
+
+
+def terrible_duplication(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
