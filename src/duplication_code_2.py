fbcdde89afa11518cd908787b72dbea49f6f0a6d
xingcixu <xingci.xu@outlook.com>
2020-06-10 16:35:29 +0800

    update duplication

--- src/duplication_code_2.py
+++ src/duplication_code_2.py
@@ -1,3 +1,19 @@
+import os
+import sys
+
+def terrible_duplication2(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
 
 
 def terrible_duplication(i, j):
