5d9fcd4fd23bf3aae3f338e52f3cab1745a0254f
xingcixu <xingci.xu@outlook.com>
2020-06-10 16:39:42 +0800

    hand shack

--- src/duplication_code_2.py
+++ src/duplication_code_2.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import flask
+
 
 def terrible_duplication2(i, j):
     if i > 100:
@@ -29,3 +31,21 @@ def terrible_duplication(i, j):
             for k in range(x):
                 x += k
     return x
+
+
+def terrible_duplication(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+
+sfasdfd
