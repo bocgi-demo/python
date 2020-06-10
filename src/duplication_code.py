ee11dd3d789de8054dc48ba65ffb67cbfc4e7b73
xxc <x007007007@hotmail.com>
2020-06-10 14:54:45 +0800

    duplication

--- src/duplication_code.py
+++ src/duplication_code.py
@@ -13,3 +13,174 @@ def terrible_duplication(i, j):
             for k in range(x):
                 x += k
     return x
+
+
+def terrible_duplication2(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication3(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+
+def terrible_duplication4(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+
+def terrible_duplication5(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication6(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication7(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication8(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication9(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication10(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication11(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication12(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
+
+def terrible_duplication13(i, j):
+    if i > 100:
+        if i % 3 < 2:
+            for j in range(i):
+                x = int(str(j) + str(i))
+                if x % 2:
+                    break
+            else:
+                x = 0
+        else:
+            for k in range(x):
+                x += k
+    return x
