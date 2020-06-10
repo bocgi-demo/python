4e0c59c9a5fff2f123aa0e1252316fcc1f0fe0aa
xingcixu <xingci.xu@outlook.com>
2020-06-10 16:50:10 +0800

    bug fix

--- src/duplication_code_2.py
+++ src/duplication_code_2.py
@@ -19,33 +19,5 @@ def terrible_duplication2(i, j):
 
 
 def terrible_duplication(i, j):
-    if i > 100:
-        if i % 3 < 2:
-            for j in range(i):
-                x = int(str(j) + str(i))
-                if x % 2:
-                    break
-            else:
-                x = 0
-        else:
-            for k in range(x):
-                x += k
-    return x
+    print("hello world")
 
-
-def terrible_duplication(i, j):
-    if i > 100:
-        if i % 3 < 2:
-            for j in range(i):
-                x = int(str(j) + str(i))
-                if x % 2:
-                    break
-            else:
-                x = 0
-        else:
-            for k in range(x):
-                x += k
-    return x
-
-
-sfasdfd
